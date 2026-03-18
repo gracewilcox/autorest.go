a4f36c37cb3a1f502ae102651949a5f7b288fe40
gracewilcox <graceawilcox@gmail.com>
2026-03-18 13:28:25 -0700

    etag progress

--- packages/typespec-go/test/local/azblob/generated/transforms/transforms.go
+++ packages/typespec-go/test/local/azblob/generated/transforms/transforms.go
@@ -36,35 +36,35 @@ func applyReplacements(fileNames []string, replacements []replacement) {
 
 // can do in generator??
 func main() {
-	// Replace ETag fields in models and responses
-	applyReplacements([]string{"models.go", "responses.go"}, []replacement{
-		{`"time"`, "\"time\"\n\t\"github.com/Azure/azure-sdk-for-go/sdk/azcore\""},
-		{`ETag\s+\*string`, `ETag *azcore.ETag`},
-	})
+	// // Replace ETag fields in models and responses
+	// applyReplacements([]string{"models.go", "responses.go"}, []replacement{
+	// 	{`"time"`, "\"time\"\n\t\"github.com/Azure/azure-sdk-for-go/sdk/azcore\""},
+	// 	{`ETag\s+\*string`, `ETag *azcore.ETag`},
+	// })
 
-	// Replace ETag fields in options
-	applyReplacements([]string{"options.go"}, []replacement{
-		{`import "time"`, "import (\n\t\"time\"\n\t\"github.com/Azure/azure-sdk-for-go/sdk/azcore\"\n)"},
-		{`IfMatch\s+\*string`, `IfMatch *azcore.ETag`},
-		{`IfNoneMatch\s+\*string`, `IfNoneMatch *azcore.ETag`},
-		{`SourceIfMatch\s+\*string`, `SourceIfMatch *azcore.ETag`},
-		{`SourceIfNoneMatch\s+\*string`, `SourceIfNoneMatch *azcore.ETag`},
-	})
+	// // Replace ETag fields in options
+	// applyReplacements([]string{"options.go"}, []replacement{
+	// 	{`import "time"`, "import (\n\t\"time\"\n\t\"github.com/Azure/azure-sdk-for-go/sdk/azcore\"\n)"},
+	// 	{`IfMatch\s+\*string`, `IfMatch *azcore.ETag`},
+	// 	{`IfNoneMatch\s+\*string`, `IfNoneMatch *azcore.ETag`},
+	// 	{`SourceIfMatch\s+\*string`, `SourceIfMatch *azcore.ETag`},
+	// 	{`SourceIfNoneMatch\s+\*string`, `SourceIfNoneMatch *azcore.ETag`},
+	// })
 
-	// Apply common client transformations for ETag handling
-	clientFiles := []string{
-		"appendblob_client.go",
-		"blob_client.go",
-		"blockblob_client.go",
-		"container_client.go",
-		"pageblob_client.go",
-	}
+	// // Apply common client transformations for ETag handling
+	// clientFiles := []string{
+	// 	"appendblob_client.go",
+	// 	"blob_client.go",
+	// 	"blockblob_client.go",
+	// 	"container_client.go",
+	// 	"pageblob_client.go",
+	// }
 
-	applyReplacements(clientFiles, []replacement{
-		{`result\.ETag\s+=\s+&val`, `result.ETag = (*azcore.ETag)(&val)`},
-		{`\*options.IfMatch`, `string(*options.IfMatch)`},
-		{`\*options.IfNoneMatch`, `string(*options.IfNoneMatch)`},
-		{`\*options.SourceIfMatch`, `string(*options.SourceIfMatch)`},
-		{`\*options.SourceIfNoneMatch`, `string(*options.SourceIfNoneMatch)`},
-	})
+	// applyReplacements(clientFiles, []replacement{
+	// 	{`result\.ETag\s+=\s+&val`, `result.ETag = (*azcore.ETag)(&val)`},
+	// 	{`\*options.IfMatch`, `string(*options.IfMatch)`},
+	// 	{`\*options.IfNoneMatch`, `string(*options.IfNoneMatch)`},
+	// 	{`\*options.SourceIfMatch`, `string(*options.SourceIfMatch)`},
+	// 	{`\*options.SourceIfNoneMatch`, `string(*options.SourceIfNoneMatch)`},
+	// })
 }
